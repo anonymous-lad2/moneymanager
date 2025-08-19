746e971186fd0bfab0b475893e487db9ba005732
anonymous-lad2 <anonymouslad540@gmail.com>
2025-08-19 23:17:49 +0530

    profile service

---
+++ moneymanager-backend/src/main/java/com/happysat/moneymanager/repository/ProfileRepository.java
@@ -0,0 +1,12 @@
+package com.happysat.moneymanager.repository;
+
+import com.happysat.moneymanager.entity.ProfileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProfileRepository extends JpaRepository<ProfileEntity, Long> {
+
+    Optional<ProfileEntity> findByEmail(String email);
+
+}
