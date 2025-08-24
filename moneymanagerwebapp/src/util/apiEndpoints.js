3e4c70e091f045cb4a46227d5dbf8f830394c815
anonymous-lad2 <anonymouslad540@gmail.com>
2025-08-24 22:24:53 +0530

    login and signup

--- moneymanagerwebapp/src/util/apiEndpoints.js
+++ moneymanagerwebapp/src/util/apiEndpoints.js
@@ -1,6 +1,8 @@
 export const BASE_URL = "https://moneymanager-backend-udtk.onrender.com/api/v1.0";
+const CLOUDINARY_CLOUD_NAME = "dxnp8ypmy";
 
 export const API_ENDPOINTS = {
     LOGIN: "/login",
-    REGISTER: "/register"
+    REGISTER: "/register",
+    UPLOAD_IMAGE: `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`
 }
